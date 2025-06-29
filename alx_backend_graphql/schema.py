efd54ceedc3147ffd4c1b5c446f0863164394aed
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-29 18:06:39 +0300

    Update schema.py to combine crm’s Query and Mutation.

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
@@ -6,4 +7,10 @@ class Query(graphene.ObjectType):
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
 
-schema = graphene.Schema(query=Query)
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
